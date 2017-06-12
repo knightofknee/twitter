734121d53911ccbc39364540190841177386be44
knightofknee <bricarlis@gmail.com>
2017-06-12 17:21:23 -0500

    an hour in, good stuff

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 "use strict";
 
+var express=require('express')
+var morgan=require('morgan')
+var volleyball=require('volleyball')
+
+const app=express()
+
+app.listen(3000,function(){
+
+  console.log('server listening')
+
+
+})
+
+app.use(volleyball)
+
+
+app.get('/',function(req,res,next){
+  console.log('get')
+
+
+  res.send('hahahaha your tweetsuck')
+
+})
+
+
+
+
